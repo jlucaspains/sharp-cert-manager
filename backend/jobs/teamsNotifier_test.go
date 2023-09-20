d6b4293cfaf40bb38b878a6c24ab3cd4c8e9a6be
jlucaspains <jlucaspains@gmail.com>
2023-09-19 18:29:20 -0600

    Jobs and teams webhook.

---
+++ backend/jobs/teamsNotifier_test.go
@@ -0,0 +1,95 @@
+package jobs
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/jlucaspains/sharp-cert-manager/models"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTeamsNotifierExplicitInit(t *testing.T) {
+	mux := http.NewServeMux()
+	ts := httptest.NewUnstartedServer(mux)
+	ts.Start()
+	defer ts.Close()
+
+	var result string
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		defer r.Body.Close()
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(r.Body)
+		result = buf.String()
+	})
+
+	teamsNotifier := &TeamsNotifier{}
+	teamsNotifier.Init(ts.URL, "title", "body", "url")
+	err := teamsNotifier.Notify([]models.CertCheckResult{})
+	assert.Nil(t, err)
+	assert.Equal(t, "{\n\t\"type\": \"message\",\n\t\"attachments\": [{\n\t\t\"contentType\": \"application/vnd.microsoft.card.adaptive\",\n\t\t\"content\": {\n\t\t\t\"type\": \"AdaptiveCard\",\n\t\t\t\"version\": \"1.5\",\n\t\t\t\"$schema\": \"http://adaptivecards.io/schemas/adaptive-card.json\",\n\t\t\t\"body\": [\n\t\t\t\t{\n\t\t\t\t\t\"type\": \"TextBlock\",\n\t\t\t\t\t\"text\": \"title\",\n\t\t\t\t\t\"size\": \"large\",\n\t\t\t\t\t\"weight\": \"bolder\",\n\t\t\t\t\t\"wrap\": true\n\t\t\t\t},\n\t\t\t\t{\n\t\t\t\t\t\"type\": \"TextBlock\",\n\t\t\t\t\t\"text\": \"body\",\n\t\t\t\t\t\"isSubtle\": true,\n\t\t\t\t\t\"wrap\": true\n\t\t\t\t},\n\t\t\t\t{\n\t\t\t\t\t\"type\": \"Table\",\n\t\t\t\t\t\"columns\": [\n\t\t\t\t\t\t{\n\t\t\t\t\t\t\t\"width\": 2\n\t\t\t\t\t\t},\n\t\t\t\t\t\t{\n\t\t\t\t\t\t\t\"width\": 4\n\t\t\t\t\t\t}\n\t\t\t\t\t],\n\t\t\t\t\t\"rows\": [\n\t\t\t\t\t]\n\t\t\t\t}\n\t\t\t],\n\t\t\t\"actions\": [\n\t\t\t\t{\n\t\t\t\t\t\"type\": \"Action.OpenUrl\",\n\t\t\t\t\t\"title\": \"View Details\",\n\t\t\t\t\t\"url\": \"url\"\n\t\t\t\t}\n\t\t\t]\n\t\t}\n\t}]\n}", result)
+}
+
+func TestTeamsNotifierImplicitInit(t *testing.T) {
+	mux := http.NewServeMux()
+	ts := httptest.NewUnstartedServer(mux)
+	ts.Start()
+	defer ts.Close()
+
+	var result string
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		defer r.Body.Close()
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(r.Body)
+		result = buf.String()
+	})
+
+	teamsNotifier := &TeamsNotifier{}
+	teamsNotifier.Init(ts.URL, "", "", "")
+	err := teamsNotifier.Notify([]models.CertCheckResult{})
+	assert.Nil(t, err)
+	assert.Equal(t, "{\n\t\"type\": \"message\",\n\t\"attachments\": [{\n\t\t\"contentType\": \"application/vnd.microsoft.card.adaptive\",\n\t\t\"content\": {\n\t\t\t\"type\": \"AdaptiveCard\",\n\t\t\t\"version\": \"1.5\",\n\t\t\t\"$schema\": \"http://adaptivecards.io/schemas/adaptive-card.json\",\n\t\t\t\"body\": [\n\t\t\t\t{\n\t\t\t\t\t\"type\": \"TextBlock\",\n\t\t\t\t\t\"text\": \"Cert Manager Check Summary\",\n\t\t\t\t\t\"size\": \"large\",\n\t\t\t\t\t\"weight\": \"bolder\",\n\t\t\t\t\t\"wrap\": true\n\t\t\t\t},\n\t\t\t\t{\n\t\t\t\t\t\"type\": \"TextBlock\",\n\t\t\t\t\t\"text\": \"The following certificates were checked on 09/19/2023\",\n\t\t\t\t\t\"isSubtle\": true,\n\t\t\t\t\t\"wrap\": true\n\t\t\t\t},\n\t\t\t\t{\n\t\t\t\t\t\"type\": \"Table\",\n\t\t\t\t\t\"columns\": [\n\t\t\t\t\t\t{\n\t\t\t\t\t\t\t\"width\": 2\n\t\t\t\t\t\t},\n\t\t\t\t\t\t{\n\t\t\t\t\t\t\t\"width\": 4\n\t\t\t\t\t\t}\n\t\t\t\t\t],\n\t\t\t\t\t\"rows\": [\n\t\t\t\t\t]\n\t\t\t\t}\n\t\t\t]\n\t\t}\n\t}]\n}", result)
+}
+
+func TestTeamsNotifierWithData(t *testing.T) {
+	mux := http.NewServeMux()
+	ts := httptest.NewUnstartedServer(mux)
+	ts.Start()
+	defer ts.Close()
+
+	var result string
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		defer r.Body.Close()
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(r.Body)
+		result = buf.String()
+	})
+
+	teamsNotifier := &TeamsNotifier{}
+	teamsNotifier.Init(ts.URL, "", "", "")
+	err := teamsNotifier.Notify([]models.CertCheckResult{
+		{Hostname: "host1", IsValid: true},
+	})
+	assert.Nil(t, err)
+	assert.Equal(t, "{\n\t\"type\": \"message\",\n\t\"attachments\": [{\n\t\t\"contentType\": \"application/vnd.microsoft.card.adaptive\",\n\t\t\"content\": {\n\t\t\t\"type\": \"AdaptiveCard\",\n\t\t\t\"version\": \"1.5\",\n\t\t\t\"$schema\": \"http://adaptivecards.io/schemas/adaptive-card.json\",\n\t\t\t\"body\": [\n\t\t\t\t{\n\t\t\t\t\t\"type\": \"TextBlock\",\n\t\t\t\t\t\"text\": \"Cert Manager Check Summary\",\n\t\t\t\t\t\"size\": \"large\",\n\t\t\t\t\t\"weight\": \"bolder\",\n\t\t\t\t\t\"wrap\": true\n\t\t\t\t},\n\t\t\t\t{\n\t\t\t\t\t\"type\": \"TextBlock\",\n\t\t\t\t\t\"text\": \"The following certificates were checked on 09/19/2023\",\n\t\t\t\t\t\"isSubtle\": true,\n\t\t\t\t\t\"wrap\": true\n\t\t\t\t},\n\t\t\t\t{\n\t\t\t\t\t\"type\": \"Table\",\n\t\t\t\t\t\"columns\": [\n\t\t\t\t\t\t{\n\t\t\t\t\t\t\t\"width\": 2\n\t\t\t\t\t\t},\n\t\t\t\t\t\t{\n\t\t\t\t\t\t\t\"width\": 4\n\t\t\t\t\t\t}\n\t\t\t\t\t],\n\t\t\t\t\t\"rows\": [\n\t\t\t\t\t\t{\n\t\t\t\t\t\t\t\"type\": \"TableRow\",\n\t\t\t\t\t\t\t\"cells\": [\n\t\t\t\t\t\t\t\t{\n\t\t\t\t\t\t\t\t\t\"type\": \"TableCell\",\n\t\t\t\t\t\t\t\t\t\"items\": [\n\t\t\t\t\t\t\t\t\t\t{\n\t\t\t\t\t\t\t\t\t\t\"type\": \"TextBlock\",\n\t\t\t\t\t\t\t\t\t\t\"text\": \"✔️host1\"\n\t\t\t\t\t\t\t\t\t\t}\n\t\t\t\t\t\t\t\t\t]\n\t\t\t\t\t\t\t\t},\n\t\t\t\t\t\t\t\t{\n\t\t\t\t\t\t\t\t\t\"type\": \"TableCell\",\n\t\t\t\t\t\t\t\t\t\"items\": [\n\t\t\t\t\t\t\t\t\t\t{\n\t\t\t\t\t\t\t\t\t\t\"type\": \"TextBlock\",\n\t\t\t\t\t\t\t\t\t\t\"text\": \"\",\n\t\t\t\t\t\t\t\t\t\t\"wrap\": true\n\t\t\t\t\t\t\t\t\t\t}\n\t\t\t\t\t\t\t\t\t]\n\t\t\t\t\t\t\t\t}\n\t\t\t\t\t\t\t]\n\t\t\t\t\t\t}\n\t\t\t\t\t]\n\t\t\t\t}\n\t\t\t]\n\t\t}\n\t}]\n}", result)
+}
+
+func TestTeamsNotifierBadResponseCode(t *testing.T) {
+	mux := http.NewServeMux()
+	ts := httptest.NewUnstartedServer(mux)
+	ts.Start()
+	defer ts.Close()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusBadRequest)
+	})
+
+	teamsNotifier := &TeamsNotifier{}
+	teamsNotifier.Init(ts.URL, "", "", "")
+	err := teamsNotifier.Notify([]models.CertCheckResult{})
+	assert.Equal(t, "error sending notification to Teams", err.Error())
+}
