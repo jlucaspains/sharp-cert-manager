b55eae1af92456a349617f662133f9a773155526
Lucas Pains <jlucaspains@gmail.com>
2025-04-05 17:12:22 -0600

    More tests (#155)

--- src/handlers/frontend_test.go
+++ src/handlers/frontend_test.go
@@ -58,6 +58,57 @@ func TestRendersItem(t *testing.T) {
 	assert.Contains(t, body, "<h2 class=\"text-white text-lg font-medium\">blog.lpains.net</h2>")
 }
 
+func TestRendersItemError(t *testing.T) {
+	templatePath = "../frontend"
+	handlers := new(Handlers)
+	handlers.CertList = []models.CheckCertItem{
+		{Name: "blo.lpains.net", Url: "https://blo.lpains.net", Type: models.CertCheckURL},
+	}
+
+	router := http.NewServeMux()
+	router.HandleFunc("GET /item", handlers.GetItem)
+
+	code, _, body, _, err := makeRequest[string](router, "GET", "/item?name=blo.lpains.net", nil)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 500, code)
+	assert.Contains(t, body, "Failed to process request")
+}
+
+func TestRendersItemNoName(t *testing.T) {
+	templatePath = "../frontend"
+	handlers := new(Handlers)
+	handlers.CertList = []models.CheckCertItem{
+		{Name: "blog.lpains.net", Url: "https://blog.lpains.net", Type: models.CertCheckURL},
+	}
+
+	router := http.NewServeMux()
+	router.HandleFunc("GET /item", handlers.GetItem)
+
+	code, _, body, _, err := makeRequest[string](router, "GET", "/item", nil)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 400, code)
+	assert.Contains(t, body, "name is required")
+}
+
+func TestRendersItemBadName(t *testing.T) {
+	templatePath = "../frontend"
+	handlers := new(Handlers)
+	handlers.CertList = []models.CheckCertItem{
+		{Name: "blog.lpains.net", Url: "https://blog.lpains.net", Type: models.CertCheckURL},
+	}
+
+	router := http.NewServeMux()
+	router.HandleFunc("GET /item", handlers.GetItem)
+
+	code, _, body, _, err := makeRequest[string](router, "GET", "/item?name=badname.bad.com", nil)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 400, code)
+	assert.Contains(t, body, "the provided cert name is not configured")
+}
+
 func TestRendersItemDetail(t *testing.T) {
 	templatePath = "../frontend"
 	handlers := new(Handlers)
@@ -74,4 +125,71 @@ func TestRendersItemDetail(t *testing.T) {
 	assert.Equal(t, 200, code)
 	// Testing the full HTML content is not practical, so we check for specific elements
 	assert.Contains(t, body, "<td class=\"px-4 py-2\">blog.lpains.net</td>")
+}
+
+func TestRendersItemDetailError(t *testing.T) {
+	templatePath = "../frontend"
+	handlers := new(Handlers)
+	handlers.CertList = []models.CheckCertItem{
+		{Name: "blo.lpains.net", Url: "https://blo.lpains.net", Type: models.CertCheckURL},
+	}
+
+	router := http.NewServeMux()
+	router.HandleFunc("GET /itemDetail", handlers.GetItemDetail)
+
+	code, _, body, _, err := makeRequest[string](router, "GET", "/itemDetail?name=blo.lpains.net", nil)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 500, code)
+	assert.Contains(t, body, "Failed to process request")
+}
+
+func TestRendersItemDetailNoName(t *testing.T) {
+	templatePath = "../frontend"
+	handlers := new(Handlers)
+	handlers.CertList = []models.CheckCertItem{
+		{Name: "blog.lpains.net", Url: "https://blog.lpains.net", Type: models.CertCheckURL},
+	}
+
+	router := http.NewServeMux()
+	router.HandleFunc("GET /itemDetail", handlers.GetItemDetail)
+
+	code, _, body, _, err := makeRequest[string](router, "GET", "/itemDetail", nil)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 400, code)
+	// Testing the full HTML content is not practical, so we check for specific elements
+	assert.Contains(t, body, "name is required")
+}
+
+func TestRendersItemDetailBadName(t *testing.T) {
+	templatePath = "../frontend"
+	handlers := new(Handlers)
+	handlers.CertList = []models.CheckCertItem{
+		{Name: "blog.lpains.net", Url: "https://blog.lpains.net", Type: models.CertCheckURL},
+	}
+
+	router := http.NewServeMux()
+	router.HandleFunc("GET /itemDetail", handlers.GetItemDetail)
+
+	code, _, body, _, err := makeRequest[string](router, "GET", "/itemDetail?name=bad.name.com", nil)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 400, code)
+	// Testing the full HTML content is not practical, so we check for specific elements
+	assert.Contains(t, body, "the provided cert name is not configured")
+}
+
+func TestRendersEmpty(t *testing.T) {
+	templatePath = "../frontend"
+	handlers := new(Handlers)
+
+	router := http.NewServeMux()
+	router.HandleFunc("GET /empty", handlers.GetEmpty)
+
+	code, _, body, _, err := makeRequest[string](router, "GET", "/empty", nil)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 200, code)
+	assert.Equal(t, body, "")
 }
